2c285d393dafa055fa7ef8a0f7f65f086491ffbc
CARLOS A M ILDEFONSO ILDEFONSO <cildefonso@gmail.com>
2020-10-01 20:53:39 -0300

    alteração do arquivo do travis.yml nome do JAR

--- src/main/java/com/bip/api/domain/service/CompanyServiceImpl.java
+++ src/main/java/com/bip/api/domain/service/CompanyServiceImpl.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.bip.api.domain.model.Company;
 import com.bip.api.domain.repository.CompanyRepository;
 import com.bip.domain.exception.NegocioException;
 
-
-public class CompanyServiceImpl implements  CompanyService{
+@Service
+public class CompanyServiceImpl implements CompanyService{
+	
 	private static final Logger log = LoggerFactory.getLogger(CompanyServiceImpl.class);
 	@Autowired
 	private CompanyRepository companyRepository;
