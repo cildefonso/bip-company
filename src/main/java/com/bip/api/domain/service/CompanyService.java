73f828f5dfe2cb63b2f5b2128b89381308c4b332
CARLOS A M ILDEFONSO ILDEFONSO <cildefonso@gmail.com>
2020-10-02 06:40:48 -0300

    alteração do arquivo do travis.yml nome do JAR

---
+++ src/main/java/com/bip/api/domain/service/CompanyService.java
@@ -0,0 +1,77 @@
+package com.bip.api.domain.service;
+
+import java.util.List;
+
+import com.bip.api.domain.model.Company;
+
+
+
+public interface CompanyService {
+
+	/**
+	 * Retorna uma lista de empresas
+	 * @param company
+	 */
+	 Company insert(Company company);
+	 
+	/**
+	 * Retorna uma lista de empresas
+	 * @param company
+	 */
+	 Company upDate(Company company);
+	 
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param company
+	 */
+ 	void deletar(Company company);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 */
+ 	long count();
+ 	
+	/**
+	 * Retorna uma lista de empresas.
+	 */
+ 	List<Company> findAll();
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param userId
+	 */
+ 	Company findByUserId(String userId);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param userId
+	 */
+ 	Company findBy_id(String userId);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param cnpj
+	 */
+ 	Company findByCnpj(String cnpj);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param email
+	 */
+ 	Company findByEmail(String email);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param fullnamecompany
+	 */
+ 	Company findByfullnamecompany(String fullnamecompany);
+ 	
+	/**
+	 * Remove um lançamento da base de dados.
+	 * @param numberInitial
+	 * @param numberFinal
+	 */
+ 	List<Company> findByNumberAddressBetween(int numberInitial, int numberFinal);
+
+
+}
